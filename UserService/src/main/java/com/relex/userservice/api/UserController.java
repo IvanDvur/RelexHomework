6e1aa56e5e62e8aac89dcb8106a47fdbb4e1b3ce
ivand <ivandvurechenskiy26@gmail.com>
2023-07-08 18:05:55 +0300

    circuit breaker implemented

--- UserService/src/main/java/com/relex/userservice/api/UserController.java
+++ UserService/src/main/java/com/relex/userservice/api/UserController.java
@@ -2,7 +2,10 @@ package com.relex.userservice.api;
 
 import com.relex.userservice.domain.User;
 import com.relex.userservice.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +16,10 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-public class UserController{
+public class UserController {
 
     private final UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @PostMapping
     public ResponseEntity<User> saveUser(@RequestBody User importedUser) {
@@ -24,20 +28,32 @@ public class UserController{
     }
 
     @GetMapping
+    @CircuitBreaker(name = "ratingServiceBreaker", fallbackMethod = "ratingServiceFallback")
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
     }
 
+
+    public ResponseEntity<User> ratingServiceFallback(Exception ex) {
+        logger.info("Сервис rating-service недоступен с ошибкой: {}", ex.getMessage());
+        return new ResponseEntity<>(new User().builder()
+                .userId(UUID.randomUUID())
+                .username("Что-то пошло не так")
+                .userRatings(null).email(null)
+                .build(), HttpStatus.BAD_REQUEST);
+
+    }
+
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingServiceBreaker", fallbackMethod = "ratingServiceFallback")
     public ResponseEntity<User> getAllUsers(@PathVariable("userId") String userId) {
         User user = userService.getUser(UUID.fromString(userId));
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @GetMapping("/getAverage")
-    public ResponseEntity<String> getAverageHotelRatings(){
+    public ResponseEntity<String> getAverageHotelRatings() {
         String avgCsv = userService.getAverageHotelRatings();
-        return new ResponseEntity<>(avgCsv,HttpStatus.OK);
+        return new ResponseEntity<>(avgCsv, HttpStatus.OK);
     }
-
 }
